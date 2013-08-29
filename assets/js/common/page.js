655300477fe779af29aefb905fc5a5ce05840dcd
dxq613 <xiaoqing.dongxq@alibaba-inc.com>
2013-08-29 18:39:38 +0800

    add lazyload for demo

--- assets/js/common/page.js
+++ assets/js/common/page.js
@@ -67,6 +67,7 @@ define('common/page',function (require) {
         return;
       }
       var _self = this,
+        apis = [],
         nodes = $(_self.get('api'));
       BUI.each(nodes,function(node){
         var _self = this,
@@ -74,7 +75,9 @@ define('common/page',function (require) {
           srcNode : node
         });
         api.render();
+        apis.push(api);
       });
+      _self.set('apis',apis);
     },
     _isHasAPI : function(){
       return $(this.get('api')).length;
@@ -82,16 +85,47 @@ define('common/page',function (require) {
     //创建demo对象
     _initDemos : function(){
       var _self = this,
+        demos = _self.get('demos'),
         demoCls = _self.get('demoCls');
       $('.' + demoCls).each(function(index,node){
         var demo = new Demo({
           srcNode : node
         });
-        demo.render();
-        demo.fetchSource();
-        _self.get('demos').push(demo);
+        //demo.render();
+        //demo.fetchSource();
+        demos.push(demo);
+      });
+      if(demos.length){
+        _self._renderDemos(demos);
+      }
+    },
+    //获取未加载的demo
+    _getUnloadDemos : function(){
+      var _self = this,
+        demos = _self.get('demos'),
+        rst = [];
+      BUI.each(demos,function(demo){
+        if(!demo.get('rendered')){
+          rst.push(demo);
+        }
+      });
+      return rst;
+    },
+    //渲染示例
+    _renderDemos : function(demos){
+      var _self = this;
+
+      BUI.each(demos,function(demo){
+        if(!demo.get('rendered')){
+          var el = demo.get('srcNode');
+          if(BUI.isInVerticalView(el.offset().top)){
+            demo.render();
+            demo.fetchSource();
+          }
+        }
       });
     },
+    //初始化导航菜单
     _initNavMenu : function(){
       var _self = this,
         hEls = $('h2,h3'),
@@ -113,6 +147,7 @@ define('common/page',function (require) {
         children : menus
       });
       menu.render();
+      _self.set('menu',menu);
     },
     _initLinks : function(){
       var _self = this,
@@ -148,7 +183,41 @@ define('common/page',function (require) {
     },
     //初始化事件
     _initEvent : function(){
+      var _self = this;
 
+      $(window).on('unload',function(){
+        _self.destroy();
+      });
+
+      function callback(){
+        var demos = _self._getUnloadDemos();
+        if(!demos.length){
+          $(window).off('scroll',callback);
+        }else{
+          _self._renderDemos(demos);
+        }
+      }
+
+      $(window).on('scroll',callback);
+    },
+    /**
+     * 页面释放
+     */
+    destroy : function(){
+      var _self = this,
+        demos = _self.get('demos'),
+        menu = _self.get('menu'),
+        apis = _self.get('apis');
+
+      BUI.each(demos,function(demo){
+        demo.destroy();
+      });
+
+      BUI.each(apis,function(api){
+        api.destroy();
+      });
+      menu.destroy();
+      _self.clearAttrVals();
     }
   });
 
